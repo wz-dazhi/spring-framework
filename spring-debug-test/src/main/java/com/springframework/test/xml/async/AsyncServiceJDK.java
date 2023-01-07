1297b91aef051fffcac2e691e8da3027149190bb
wz-dazhi <742820195@qq.com>
2023-01-07 14:05:51 +0800

    test async

---
+++ spring-debug-test/src/main/java/com/springframework/test/xml/async/AsyncServiceJDK.java
@@ -0,0 +1,15 @@
+package com.springframework.test.xml.async;
+
+/**
+ * @projectName: spring-framework
+ * @package: com.springframework.test.xml.async
+ * @className: AsyncServiceJDK
+ * @description:
+ * @author: zhi
+ * @date: 2022/10/13
+ * @version: 1.0
+ */
+public interface AsyncServiceJDK {
+
+	void m(String s);
+}

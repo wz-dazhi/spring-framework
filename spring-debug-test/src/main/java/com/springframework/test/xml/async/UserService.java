5124af5a78398f36191545706f8ef1e129869084
wz-dazhi <742820195@qq.com>
2023-01-07 14:06:06 +0800

    test async

---
+++ spring-debug-test/src/main/java/com/springframework/test/xml/async/UserService.java
@@ -0,0 +1,15 @@
+package com.springframework.test.xml.async;
+
+/**
+ * @projectName: spring-framework
+ * @package: com.springframework.test.xml.async
+ * @className: UserService
+ * @description:
+ * @author: zhi
+ * @date: 2022/10/13
+ * @version: 1.0
+ */
+public interface UserService {
+
+	void say(String s);
+}

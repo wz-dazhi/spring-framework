83f422461697a686340f41e8628ba677ffcbc0b9
wz-dazhi <742820195@qq.com>
2023-03-03 15:06:40 +0800

    测试ServiceLocatorFactoryBean

---
+++ spring-debug-test/src/main/java/com/springframework/test/xml/factorybean/BankService.java
@@ -0,0 +1,16 @@
+package com.springframework.test.xml.factorybean;
+
+/**
+ * @projectName: spring-framework
+ * @package: com.springframework.test.xml.factorybean
+ * @className: BankService
+ * @description:
+ * @author: zhi
+ * @date: 2023/3/1
+ * @version: 1.0
+ */
+public interface BankService {
+
+	String pay();
+
+}
